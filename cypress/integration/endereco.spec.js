11e7695834128bad05aaddcdce7817635cfc85ad
p0ka <p0kkaxd@gmail.com>
2021-11-17 16:54:11 -0300

    massa de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 const perfil = require('../fixtures/perfil.json')
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereçps - Faturamento e Entrega', () => {
        beforeEach(() => {
@@ -12,8 +13,25 @@ describe('Funcionalidade Endereçps - Faturamento e Entrega', () => {
      })
         });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
          EnderecoPage.editarEnderecoFaturamento('Tanha', 'Mae', 'CATAL', 'Brasil', 'Rua das Flores', '47', 'Sorocaba', 'São Paulo', '18044320', '(15)99690-3344', 'emailddamae@emmail.com')
          cy.get('.woocommerce-message').should('contain' , 'Endereço alterado')
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefono,
+            dadosEndereco[2].email
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado')
+   });
 });
